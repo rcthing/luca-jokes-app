3bef0a5d01dfe440df810a559cd33c2a749f9ecd
Rednic Luca <35105055+rcthing@users.noreply.github.com>
2024-07-25 09:23:44 +0300

    Add files via upload

---
+++ src/main/java/guru/springframework/lucajokesapp/LucaJokesAppApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.lucajokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucaJokesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LucaJokesAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/lucajokesapp/LucaJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.lucajokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucaJokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
